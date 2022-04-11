a5a703ba268ec0c814bc84171b5aae20e38287aa
John Jaime Ramirez Cortes - Ceiba Software <john.ramirez@ceiba.com.co>
2022-04-11 12:10:11 -0500

    crear operaciones en comando controlador

--- microservicio/infraestructura/src/main/java/com/ceiba/articulo/controlador/ComandoControladorArticulo.java
+++ microservicio/infraestructura/src/main/java/com/ceiba/articulo/controlador/ComandoControladorArticulo.java
@@ -1,25 +1,48 @@
 package com.ceiba.articulo.controlador;
 
+import com.ceiba.ComandoRespuesta;
+import com.ceiba.articulo.ComandoArticulo;
 import com.ceiba.articulo.comando.manejador.ManejadorActualizarArticulo;
 import com.ceiba.articulo.comando.manejador.ManejadorCrearArticulo;
 import com.ceiba.articulo.comando.manejador.ManejadorEliminarArticulo;
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/articulos")
-@Api(tags = { "Controlador comando articulo"})
+@Api(tags = { "Controlador comando artículo"})
 public class ComandoControladorArticulo {
 
-    private final ManejadorCrearArticulo manejadorCrearLibro;
-    private final ManejadorEliminarArticulo manejadorEliminarLibro;
-    private final ManejadorActualizarArticulo manejadorActualizarLibro;
+    private final ManejadorCrearArticulo manejadorCrearArticulo;
+    private final ManejadorEliminarArticulo manejadorEliminarArticulo;
+    private final ManejadorActualizarArticulo manejadorActualizarArticulo;
 
-    public ComandoControladorArticulo(ManejadorCrearArticulo manejadorCrearLibro, ManejadorEliminarArticulo manejadorEliminarLibro, ManejadorActualizarArticulo manejadorActualizarLibro) {
-        this.manejadorCrearLibro = manejadorCrearLibro;
-        this.manejadorEliminarLibro = manejadorEliminarLibro;
-        this.manejadorActualizarLibro = manejadorActualizarLibro;
+    @Autowired
+    public ComandoControladorArticulo(ManejadorCrearArticulo manejadorCrearArticulo, ManejadorEliminarArticulo manejadorEliminarArticulo, ManejadorActualizarArticulo manejadorActualizarArticulo) {
+        this.manejadorCrearArticulo = manejadorCrearArticulo;
+        this.manejadorEliminarArticulo = manejadorEliminarArticulo;
+        this.manejadorActualizarArticulo = manejadorActualizarArticulo;
+    }
+
+    @PostMapping
+    @ApiOperation("Crear artículo")
+    public ComandoRespuesta<Long> crear(@RequestBody ComandoArticulo comandoArticulo) {
+        return manejadorCrearArticulo.ejecutar(comandoArticulo);
+    }
+
+    @PutMapping(value="/{id}")
+    @ApiOperation("Actualizar artículo")
+    public void actualizar(@RequestBody ComandoArticulo comandoArticulo,@PathVariable Long idArticulo) {
+        comandoArticulo.setIdArticulo(idArticulo);
+        manejadorActualizarArticulo.ejecutar(comandoArticulo);
+    }
+
+    @DeleteMapping(value="/{id}")
+    @ApiOperation("Eliminar artículo")
+    public  ComandoRespuesta<Long> eliminar(@PathVariable Long id) {
+        return manejadorEliminarArticulo.ejecutar(id);
     }
 
 }
